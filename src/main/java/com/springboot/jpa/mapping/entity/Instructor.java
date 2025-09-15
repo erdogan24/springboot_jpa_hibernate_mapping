63c505012194b53db0a85552dc840cf6c3f4971b
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-15 21:59:10 +0300

    first commit

---
+++ src/main/java/com/springboot/jpa/mapping/entity/Instructor.java
@@ -0,0 +1,34 @@
+package com.springboot.jpa.mapping.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+
+    // annotate the class as an entity and map to db table
+
+    // define the fields
+
+    // annotate the fields with db column names
+
+    //create constructors
+
+    // generate getter/setter method
+
+    // generate toString() method
+}
+
+
+
+
+
+
+
+
+
+
+
+
