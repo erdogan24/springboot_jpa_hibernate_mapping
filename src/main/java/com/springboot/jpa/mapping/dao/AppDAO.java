968299d38795ae08fd013e4a13832ce5b7ab3aeb
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-15 22:31:11 +0300

    dao layer has added.

---
+++ src/main/java/com/springboot/jpa/mapping/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.springboot.jpa.mapping.dao;
+
+import com.springboot.jpa.mapping.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+}
---
+++ src/main/java/com/springboot/jpa/mapping/dao/AppDAOImpl.java
@@ -0,0 +1,26 @@
+package com.springboot.jpa.mapping.dao;
+
+import com.springboot.jpa.mapping.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDAO{
+
+    // define field for entity manager
+
+    private EntityManager entityManager;
+
+
+    // inject entity  manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
