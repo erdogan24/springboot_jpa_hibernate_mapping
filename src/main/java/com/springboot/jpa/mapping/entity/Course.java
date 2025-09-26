cadb87c01fd14e37d6cab99163f8bfc310e65c2d
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-26 23:03:45 +0300

    commit

--- src/main/java/com/springboot/jpa/mapping/entity/Course.java
+++ src/main/java/com/springboot/jpa/mapping/entity/Course.java
@@ -18,13 +18,42 @@ public class Course {
 
     // annotate fields
 
+    private int id;
+
+    private String title;
+
+    private Instructor instructor;
 
 
+    public Course (){
 
+    }
 
+    public Course(String title) {
+        this.title = title;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
 }
