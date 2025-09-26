b26b439aea3de77f06f5db7aa3683251cbe5382e
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-26 23:01:22 +0300

    course field has added.

---
+++ src/main/java/com/springboot/jpa/mapping/entity/Course.java
@@ -0,0 +1,30 @@
+package com.springboot.jpa.mapping.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    // define our fields
+
+    // define constructor
+
+    // define getter setters
+
+    // define toString
+
+    // annotate fields
+
+
+
+
+
+
+
+
+
+
+}
